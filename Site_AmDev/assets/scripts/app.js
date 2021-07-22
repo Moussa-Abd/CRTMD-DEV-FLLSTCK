732494ff98d22a71a4a16fc6a4d62350c131e7b1
Abd git <liongon@outlook.fr>
2021-07-22 20:39:09 +0200

    work in nav

--- Site_AmDev/assets/scripts/app.js
+++ Site_AmDev/assets/scripts/app.js
@@ -1,29 +1,33 @@
 // Nav scrollTop
 
 let lastScrollTop = 0;
-    header = document.querySelector(".header");
-window.addEventListener("scroll", function(){
+header = document.querySelector(".header");
+window.addEventListener("scroll", function () {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    if (scrollTop > lastScrollTop){
-        header.style.top = "-60px";
+    if (scrollTop > lastScrollTop) {
+
+	    header.style.top = "-60px";
+        
     } else {
         header.style.top = 0;
     }
-    lastScrollTop = scrollTop;   
+    header.style.transitionDelay = "0.5s";
+    lastScrollTop = scrollTop;
 })
 
 //ArrowTop
 
 let topBtn = document.querySelector(".fa-chevron-up");
-window.onscroll = function(){
+window.onscroll = function () {
     scrollFunction()
 };
-function scrollFunction(){
-    if(document.body.scroll > 100 || document.documentElement.scrollTop > 100){
-        topBtn.style.display= "block";
-        topBtn.style.transition= ".5s";
+
+function scrollFunction() {
+    if (document.body.scroll > 100 || document.documentElement.scrollTop > 100) {
+        topBtn.style.display = "block";
+        topBtn.style.transition = ".5s";
     } else {
-        topBtn.style.display="none";
+        topBtn.style.display = "none";
     }
 }
 
@@ -31,14 +35,13 @@ function scrollFunction(){
 
 let icon = document.querySelector(".icon");
 
-icon.onclick = function(){
+icon.onclick = function () {
     document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        icon.src="images/sun.png";
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "images/sun.png";
     } else {
-        icon.src="images/moon.png";
+        icon.src = "images/moon.png";
     }
 }
 
 //https://www.youtube.com/watch?v=9LZGB3OLXNQ
-

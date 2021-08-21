1581f57513481d76b4f982c3e80b4f44e8738082
Abd git <liongon@outlook.fr>
2021-08-21 15:43:24 +0200

    formulair de contact configuré

---
+++ Envoie email/assets/app.js
@@ -0,0 +1,58 @@
+
+function validate() {
+    let name = document.querySelector(".username");
+    let email = document.querySelector(".email");
+    let msg = document.querySelector(".message");
+    let btn = document.querySelector(".submit");
+
+    btn.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (name.value == "" || email.value == "" || msg.value == "") {
+
+            emptyerror();
+
+        } else { /*(name.value !== "" || email.value !== "" || msg.value !== "")*/
+
+            sendmail(name.value, email.value, msg.value);
+            success();
+
+            name.value = "";
+            email.value = "";
+            msg.value = "";
+        }
+    });
+}
+validate();
+
+function sendmail(name, email, msg){
+    emailjs.send("service_bj1y8vn","template_as5swkt", {
+    to_name: name,
+    from_name: email,
+    message: msg,
+    });
+}
+  
+
+function emptyerror() {
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Veuillez remplir tous les champs",
+    });
+}
+
+// function error() {
+//     Swal.fire({
+//         icon: "error",
+//         title: "Oops...",
+//         text: "Something went wrong!",
+//     });
+// }
+
+function success() {
+    Swal.fire({
+        icon: "success",
+        title: "Envoyé",
+        text: "Message envoyé avec succès",
+    });
+}
